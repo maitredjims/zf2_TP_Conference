4cf56c848be5b12a77bfe414389d3c7bc5813d17
maitredjims <darel.djimbi@hotmail.fr>
2015-04-28 16:08:26 +0200

    utilisation d'ObjectSelect de DoctrineModule pour le lieu

--- module/Conference/src/Conference/Form/ConferenceForm.php
+++ module/Conference/src/Conference/Form/ConferenceForm.php
@@ -2,30 +2,102 @@
 
 namespace Conference\Form;
 
-class ConferenceForm extends \Zend\Form\Form
-{
-    public function __construct() {
+use Doctrine\ORM\EntityManager;
+use Zend\Form\Form;
+
+class ConferenceForm extends Form {
+
+    protected $entityManager;
+
+    public function __construct(EntityManager $entityManager) {
         parent::__construct('conference');
-        
+
+        $this->entityManager = $entityManager;
+
         // Gestion de l'arrayCopy
         $this->setHydrator(new \Zend\Stdlib\Hydrator\ClassMethods());
-        
+
         $element = new \Zend\Form\Element\Text('nom');
         $element->setLabel('Nom de la conférence :');
         $this->add($element);
-                
+
         $element = new \Zend\Form\Element\Textarea('descriptif');
         $element->setLabel('Description de la conférence :');
         $this->add($element);
-        
+
         $element = new \Zend\Form\Element\DateTime('date_debut');
         $element->setLabel('Date de début');
         $element->setFormat('Y-m-d');
         $this->add($element);
-        
-        $element = new \Zend\Form\Element\Select('lieu_id');
-        $element->setLabel('Lieu de la conférence :');
+
+
+//        $element = new \Zend\Form\Element\Select('lieu_id');
+//        $element->setLabel('Lieu de la conférence :');
+//        $element->setDisableInArrayValidator(true);
+//        $this->add($element);
+
+        $element = new \DoctrineModule\Form\Element\ObjectSelect('lieu');
         $element->setDisableInArrayValidator(true);
+        $element->setLabel('Lieu de la conférence :');
+        $element->setOptions(array(
+            'object_manager'     => $this->entityManager,
+            'label' => 'Lieu de la conférence',
+            'target_class' => 'Conference\Entity\Lieu',
+            'property' => 'lieu',
+            'is_method' => true,
+            'find_method' => array(
+                'name' => 'getLieu',
+            ),
+        ));
         $this->add($element);
+
+//        $this->add(array(
+//           'name' => 'lieu_id',
+//           'type' => 'DoctrineModule\Form\Element\ObjectSelect',
+//           'options' => array(
+//               'label' => 'Lieu de la conférence',
+//               'target_class' => 'Conference\Entity\Lieu',
+//               'property' => 'lieu_id',
+//               'is_method' => true,
+//               'find_method' => array(
+//                   'name' => 'getLieu',
+//               ),
+//           ),
+//       ));
     }
+
+    /*
+      public function init() {
+      // Gestion de l'arrayCopy
+      $this->setHydrator(new \Zend\Stdlib\Hydrator\ClassMethods());
+
+      $element = new \Zend\Form\Element\Text('nom');
+      $element->setLabel('Nom de la conférence :');
+      $this->add($element);
+
+      $element = new \Zend\Form\Element\Textarea('descriptif');
+      $element->setLabel('Description de la conférence :');
+      $this->add($element);
+
+      $element = new \Zend\Form\Element\DateTime('date_debut');
+      $element->setLabel('Date de début');
+      $element->setFormat('Y-m-d');
+      $this->add($element);
+
+      $this->add(array(
+      'name' => 'lieu_id',
+      'type' => 'DoctrineModule\Form\Element\ObjectSelect',
+      'options' => array(
+      'object_manager' => $this->entityManager,
+      'label' => 'Lieu de la conférence',
+      'target_class' => 'Conference\Entity\Lieu',
+      'property' => 'nom',
+      'is_method' => true,
+      'find_method' => array(
+      'name' => 'getLieu',
+      ),
+      ),
+      ));
+      }
+     */
 }
