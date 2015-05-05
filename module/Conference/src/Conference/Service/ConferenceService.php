2b200927e9a3d5a1a85905ffea8c33a3ffb1be69
maitredjims <darel.djimbi@hotmail.fr>
2015-05-05 13:17:43 +0200

    modif udpdate + getById

--- module/Conference/src/Conference/Service/ConferenceService.php
+++ module/Conference/src/Conference/Service/ConferenceService.php
@@ -8,51 +8,59 @@ use Doctrine\ORM\EntityManager;
 use DoctrineModule\Stdlib\Hydrator\DoctrineObject;
 use Zend\Form\Form;
 
-class ConferenceService
-{
+class ConferenceService {
+
     /**
      *
      * @var EntityManager
      */
     protected $em;
-    
+
     public function __construct(EntityManager $em) {
         $this->em = $em;
     }
-    
+
     public function getAll() {
         $repository = $this->em->getRepository('Conference\Entity\Conference');
-        
+
         return $repository->findAll();
     }
-    
-    public function getById($id) {
+
+    public function getById($id, $form=null) {
         $repository = $this->em->getRepository('Conference\Entity\Conference');
-        
-        return $repository->find($id);
+
+        $repoFind = $repository->find($id);
+
+        if ($form != null) {
+            $hydrator = new DoctrineObject($this->em);
+            $form->setHydrator($hydrator);
+            $form->bind($repoFind);
+        }
+
+        return $repoFind;
     }
-    
+
     public function getByIdWithLieu($id) {
         $dql = "SELECT c, l FROM Conference\Entity\Conference LEFT JOIN c.lieu l WHERE c.id = :id";
-        
+
         return $this->em->createQuery($dql)
                         ->setParameter('id', $id)
                         ->getSingleResult();
     }
-    
+
     public function insert(Form $form, $dataAssoc) {
         $conference = new Conference();
-        
+
         $hydrator = new DoctrineObject($this->em);
-        
-        $form->setHydrator($hydrator);        
+
+        $form->setHydrator($hydrator);
         $form->bind($conference);
         $form->setInputFilter(new ConferenceInputFilter());
         $form->setData($dataAssoc);
         //var_dump($form); 
         //var_dump($form->bind($conference));
         //exit();
-        
+
         if (!$form->isValid()) {
             return null;
         }
@@ -62,38 +70,37 @@ class ConferenceService
         //exit();
         $this->em->persist($conference);
         $this->em->flush();
-        
+
         return $conference;
-        
     }
-    
+
     public function delete($id) {
         $conference = $this->em->find('Conference\Entity\Conference', $id);
-       
+
         $this->em->remove($conference);
         $this->em->flush();
-        
+
         return $conference;
     }
-    
-    public function update($id, Form $form, $dataAssoc) {
-        $conference = $this->em->find('Conference\Entity\Conference', $id);
-        
+
+    public function update($conference, Form $form, $dataAssoc) {
+        //$conference = $this->em->find('Conference\Entity\Conference', $id);
+
         $hydrator = new DoctrineObject($this->em);
-        
+
         $form->setHydrator($hydrator);
         $form->bind($conference);
         $form->setInputFilter(new ConferenceInputFilter());
         $form->setData($dataAssoc);
-        
+
         if (!$form->isValid()) {
             return null;
-        }     
-        
+        }
+
         $this->em->persist($conference);
         $this->em->flush();
-        
+
         return $conference;
     }
-    
+
 }
